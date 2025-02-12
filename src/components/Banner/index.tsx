ac1f3eff7649faeda44d1621b9db8ad917d87b79
wanrif <ridwansyarif23@gmail.com>
2025-02-12 19:38:13 +0700

    chore: update dependencies and update CV link in Banner and Contact components

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,10 +1,10 @@
+import Tooltip from '@components/Tooltip';
 import { motion } from 'framer-motion';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { GiAce, GiCat, GiLinkedRings } from 'react-icons/gi';
 import { GoMention } from 'react-icons/go';
 import ParticleBackground from './ParticleBackground';
-import Tooltip from '@components/Tooltip';
 
 interface ISocialLink {
   href: string;
@@ -24,7 +24,7 @@ const Banner: React.FC = () => {
     },
     { href: 'https://github.com/wanrif', icon: <GiCat className='w-5 h-5' />, tooltipText: 'Github' },
     {
-      href: 'https://drive.proton.me/urls/BR24YA9V6M#Ggg6OmLV3baa',
+      href: 'https://drive.proton.me/urls/253KWW5VM4#Tw7dKlEuPOPr',
       icon: <GiAce className='w-5 h-5' />,
       tooltipText: 'CV',
     },
@@ -38,12 +38,14 @@ const Banner: React.FC = () => {
         className='relative z-10 flex flex-col items-center justify-center text-center max-w-4xl px-4'
         initial={{ opacity: 0, y: 30 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8 }}>
+        transition={{ duration: 0.8 }}
+      >
         <motion.div
           className='mb-8'
           initial={{ scale: 0.8, opacity: 0 }}
           animate={{ scale: 1, opacity: 1 }}
-          transition={{ delay: 0.2, duration: 0.5 }}>
+          transition={{ delay: 0.2, duration: 0.5 }}
+        >
           <img
             src='/personal_photo.jpg'
             alt='Redwan'
@@ -55,7 +57,8 @@ const Banner: React.FC = () => {
           className='space-y-4 mb-8'
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ delay: 0.4, duration: 0.5 }}>
+          transition={{ delay: 0.4, duration: 0.5 }}
+        >
           <h1 className='text-5xl font-bold text-tuna-950 dark:text-gallery-100'>Redwan Sarif</h1>
           <p className='text-2xl font-medium text-tuna-800 dark:text-gallery-200'>Fullstack Developer</p>
           <p className='text-lg italic text-tuna-700 dark:text-gallery-300 max-w-2xl mx-auto'>
@@ -67,7 +70,8 @@ const Banner: React.FC = () => {
           className='flex gap-4'
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ delay: 0.6, duration: 0.5 }}>
+          transition={{ delay: 0.6, duration: 0.5 }}
+        >
           {socialLinks.map((link, index) => (
             <Tooltip key={index} text={link.tooltipText}>
               <motion.a
@@ -76,7 +80,8 @@ const Banner: React.FC = () => {
                 rel='noreferrer'
                 className='flex items-center justify-center w-10 h-10 rounded-xl bg-tertiary-400/20 hover:bg-tertiary-400/40 dark:bg-tertiary-500/20 dark:hover:bg-tertiary-500/40 text-tertiary-600 dark:text-tertiary-300 transition-all duration-300'
                 whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.95 }}>
+                whileTap={{ scale: 0.95 }}
+              >
                 {link.icon}
               </motion.a>
             </Tooltip>
